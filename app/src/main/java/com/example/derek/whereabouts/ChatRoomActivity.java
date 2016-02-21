ad95db38f6f66fccfb19ec1a4a7cefaa487dbb78
Matthew Sternberg <msternberg6@gatech.edu>
2016-02-20 22:52:59 -0600

    Map Marker icon indicates you can use the map inside a chat

--- app/src/main/java/com/example/derek/whereabouts/ChatRoomActivity.java
+++ app/src/main/java/com/example/derek/whereabouts/ChatRoomActivity.java
@@ -2,64 +2,28 @@ package com.example.derek.whereabouts;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class ChatRoomActivity extends AppCompatActivity {
+public class ChatRoomActivity extends ActionBarActivity {
 
     int room;
     String roomName;
     String username;
 
     private String[] menuOptions;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        menuOptions = new String[]{"Chats", "Map"};
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, menuOptions));
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-        setupDrawer();
 
         Intent intent = getIntent();
         room = intent.getIntExtra("ROOM_ID", -1);
         roomName = intent.getStringExtra("ROOM_NAME");
         username = intent.getStringExtra("USERNAME");
         setTitle("Room " + roomName);
-    }
-
-        private void setupDrawer() {
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
-
-            public void onDrawerOpened(View view) {
-                super.onDrawerOpened(view);
-                invalidateOptionsMenu();
-            }
-
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                invalidateOptionsMenu();
-            }
-        };
-
-        mDrawerToggle.setDrawerIndicatorEnabled(true);
-            mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     @Override
@@ -75,26 +39,14 @@ public class ChatRoomActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        if (id == R.id.action_map) {
+            Intent intent = new Intent(this, MapsActivity.class);
+            intent.putExtra("ROOM_ID", room);
+            intent.putExtra("ROOM_NAME", roomName);
+            intent.putExtra("USERNAME", username);
+            startActivity(intent);
+        }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        public void onItemClick(AdapterView parent, View view, int position, long id) {
-            if (position == 1) {
-                Intent intent = new Intent(view.getContext(), MapsActivity.class);
-                intent.putExtra("ROOM_ID", room);
-                intent.putExtra("ROOM_NAME", roomName);
-                intent.putExtra("USERNAME", username);
-                startActivity(intent);
-            }
-        }
     }
 }
